aa2018f8fddd58785b6499314c44504a74913a4d
Matthew Waite <matthewwaite@Matthews-MacBook-Pro.local>
2022-01-22 18:06:27 +0000

    extracted the transform code and put into a package in ../transform

--- tools/mongo-tools.go
+++ tools/mongo-tools.go
@@ -7,121 +7,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/m-h-w/nmea-logger/transform"
 )
 
-// This should probably be in a separate transform module.
-
-// Transform data from the B&G logger into a format that Mongo (or another timeseries DB) can
-// work with and remove all the extraneous feilds from the data captured by the logger.
-
-// The document metadat contains the only fields that can be changed once the document is written
-// more info: https://docs.mongodb.com/manual/core/timeseries/timeseries-limitations/
-
-type boatSpeedMetadata_t struct {
-	DataSource         string  `json:"source"`
-	CorrectedBoatSpeed float64 `json:"correctedBoatSpeed"`
-}
-
-type boatSpeed_t struct {
-	Ts                 string              `json:"ts"`                 // timestamp
-	Metadata           boatSpeedMetadata_t `json:"metadata"`           // Information about the reading and any corrections
-	IndicatedBoatSpeed float64             `json:"indicatedBoatSpeed"` // indicated boatspeed from the log
-}
-
-func transformSpeed(input map[string]interface{}, output *os.File) {
-
-	var boatSpeed boatSpeed_t
-
-	// map input data to boatSpeed_t var
-	boatSpeed.Ts = convertToDateFormat(input["timestamp"].(string))
-	boatSpeed.Metadata.DataSource = "log"
-
-	fields := input["fields"].(map[string]interface{})
-	boatSpeed.IndicatedBoatSpeed = fields["Speed Water Referenced"].(float64)
-
-	// build json object and write to file
-	data, _ := json.Marshal(boatSpeed)
-	_, e := output.Write(data) // write json to file
-
-	check(e)
-
-	if debug == true {
-		fmt.Printf("json: %s\r\n", data)
-	}
-
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func transformToMongoformat(ipfile string) {
-
-	// Try to open the named input file
-	ifile, err := os.Open(ipfile)
-	check(err)
-
-	// Check if output file exisits and delete it if it does.
-	if _, err := os.Stat(ipfile + ".mong"); err == nil {
-
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "error check if file exists", err)
-			os.Exit(1)
-		}
-
-		err := os.Remove(ipfile + ".mong")
-		check(err)
-	}
-
-	// Try to open the output file and append the .mong extension to it
-	ofile, err := os.Create(ipfile + ".mong")
-	check(err)
-
-	// Close files on exit of this function
-	defer ifile.Close()
-	defer ofile.Close()
-
-	// Get next line of file
-	scanner := bufio.NewScanner(ifile)
-
-	for scanner.Scan() {
-		//create a map of strings to empty interfaces to unmarshall json B&G logger data into
-		var result map[string]interface{}
-
-		//unmarshall the B&G data. Based on https://www.sohamkamani.com/golang/parsing-json/
-		err := json.Unmarshal([]byte(scanner.Text()), &result)
-
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "error unmarshalling logger data", err)
-			continue // error in the input data so skip this iteration.
-		}
-
-		switch result["description"] {
-
-		case "Speed":
-			transformSpeed(result, ofile)
-
-		default:
-			continue // skip this row as we dont want it stored in the DB
-
-		}
-
-	} //iterate until EOF or error
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading input file:", err)
-		os.Exit(1)
-	}
-
-}
-
-// end of possible module.
-
-//----------------------------------------------------------------------------------------------
-
-var debug bool = true
+//This code is a test harness that drives the various modules that transform data and write/read from the DB
+//using the command line for input.
 
 // structure containing any settings entered from the command line
 type commandLineSettings_t struct {
@@ -149,42 +40,6 @@ func parseCommandLine() *commandLineSettings_t {
 	settings.dispFields = *dispFieldPtr
 
 	return settings
-}
-
-func convertToDateFormat(date string) string {
-
-	// date string is in this format: 	2021-07-09-13:40:59.530"
-	// and needs to be in this format:	2021-07-09T13:40:59.530Z"
-
-	date += "Z"
-	byteArray := []byte(date)
-	byteArray[10] = 0x54 // ascii for 'T' at the 10th (starting from 0) position in the array
-
-	return string(byteArray)
-}
-
-// based on https://www.sohamkamani.com/golang/parsing-json/
-func modifyTimeStamp(jsonString string) (string, error) {
-
-	//create a map of strings to empty interfaces:
-	var result map[string]interface{}
-	err := json.Unmarshal([]byte(jsonString), &result)
-
-	if err != nil {
-		return "", err
-	}
-	if result["timestanp"] != nil {
-		// If it exists, the object stored in the "timestamp" key is a string
-		date := result["timestamp"].(string)
-
-		// Convert "2021-07-09-13:40:59.530" to standard date format
-		date = convertToDateFormat(date)
-		result["timestamp"] = date
-
-		newJson, err := json.Marshal(result)
-		return string(newJson), err
-	}
-	return jsonString, err //ToDo this is an error state - need to handle better.
 }
 
 // looks at the json data from the logger and counts the number of unique descriptions. Descriptions are
@@ -291,7 +146,7 @@ func main() {
 		if settings.file != "" {
 
 			fmt.Printf("transforming file to MongDB format\r\n")
-			transformToMongoformat(settings.file)
+			transform.TransformToMongoFormat(settings.file) // uses the function in the transform module
 			os.Exit(1)
 
 		} else {
