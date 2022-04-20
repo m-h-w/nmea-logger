fc4af98e0f8c187578625caca6b0ff020e91df8c
Matthew Waite <matthew.waite@klarna.com>
2022-04-20 11:46:45 +0100

    added sialnjord to command line

--- tools/mongo-tools.go
+++ tools/mongo-tools.go
@@ -20,6 +20,7 @@ type commandLineSettings_t struct {
 	transform  bool   // transforms the data from the b&g logger to a more db friendly format
 	dispFields bool   // display the different data fields in the logger data
 	file       string // take input from a file: -file <filename>
+	sailNjord  bool   // convert B&G output file to Sailnjord format for core readings
 }
 
 func parseCommandLine() *commandLineSettings_t {
@@ -29,6 +30,7 @@ func parseCommandLine() *commandLineSettings_t {
 	transformPtr := flag.Bool("t", false, "transform the input file to a db friendly format")
 	dispFieldPtr := flag.Bool("f", false, "Display the different data fields contained in the input file")
 	fileNamePtr := flag.String("file", "", "take input from a file -file <filename> ")
+	sailNjordPtr := flag.Bool("sn", false, "transform fileinput to Sail Njord format")
 
 	flag.Parse()
 
@@ -38,6 +40,7 @@ func parseCommandLine() *commandLineSettings_t {
 	settings.transform = *transformPtr
 	settings.file = *fileNamePtr
 	settings.dispFields = *dispFieldPtr
+	settings.sailNjord = *sailNjordPtr
 
 	return settings
 }
@@ -106,6 +109,48 @@ func countDescriptions(filename string) map[string]int {
 	return m
 }
 
+/*
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// The Pi doesnt have a realtime clock and so if it is powered down and back up again without access to wifi
+// it loses its time sync. When data is collected it will have the wrong time stamp on it and so we need to correct
+// it. Luckily the incomping data stream has a system time value which is sent every second in the "Sytem Time" decription
+// This functions looks for the first System time and compares it with the attached time stamp on the document, calculates
+// the error and applies it to all the documents in the file.
+
+func timeAlign(ipfile string) {
+
+	// Try to open the named input file
+	ifile, err := os.Open(ipfile)
+	check(err)
+
+	// Close file on exit of this function
+	defer ifile.Close()
+
+	//  Scan the input file.
+	scanner := bufio.NewScanner(ifile)
+
+	for scanner.Scan() {
+
+		//create a map of strings to empty interfaces to unmarshall json B&G logger data into
+		var result map[string]interface{}
+
+		//unmarshall the B&G data. Based on https://www.sohamkamani.com/golang/parsing-json/
+		err := json.Unmarshal([]byte(scanner.Text()), &result) //NB result passed by refence
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error unmarshalling logger data", err)
+			continue // error in the input data format so skip this iteration and move on.
+		}
+
+	}// itereate until EOF or error
+}
+*/
 func main() {
 
 	settings := parseCommandLine()
@@ -145,6 +190,23 @@ func main() {
 		} else {
 
 			fmt.Printf(" -t must be used in conjunction with -file <filename>\r\n")
+			os.Exit(1)
+
+		}
+	}
+
+	// Create a Sail Njord compatible file
+
+	if settings.sailNjord {
+		if settings.file != "" {
+
+			fmt.Printf("transforming file to Sail Njord format\r\n")
+			transform.SailNjordConverter(settings.file) // uses the function in the transform module
+			os.Exit(1)
+
+		} else {
+
+			fmt.Printf(" -sn must be used in conjunction with -file <filename>\r\n")
 			os.Exit(1)
 
 		}
